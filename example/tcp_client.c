399b170d6e4877f06fb067b3f8594d6965a6d6af
Denys Haryachyy <garyachy@gmail.com>
2018-02-27 18:52:57 +0200

    Added stats to TCP client.

--- example/tcp_client.c
+++ example/tcp_client.c
@@ -25,12 +25,33 @@ int sockfd;
 static char buff[100] = "";
 static uint32_t counter = 0;
 
+static unsigned long long num_bytes = 0;
+static struct timeval  tv1, tv2;
+
 int send_loop()
 {
     int ret = 0;
+
+    if (num_bytes == 0)
+            {
+                gettimeofday(&tv1, NULL);
+            }
+
+
     ret = snprintf(buff, sizeof(buff), "%u,",  counter);
     ff_write(sockfd, buff, ret);
+    num_bytes += ret; 
     counter++;
+
+            if (num_bytes == 1288890)
+            {
+                gettimeofday(&tv2, NULL);
+                float secs = (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 + (double) (tv2.tv_sec - tv1.tv_sec);
+                printf("Sent %u messages, %u bytes, %f sec, %f B/s, %f Msg/sec \n",
+                       counter, num_bytes, secs, num_bytes/secs, counter/secs);
+            }
+
+
 }
 
 static unsigned long cur_count = 0;
@@ -46,10 +67,6 @@ int loop(void *arg)
     send_loop();
     cur_count++;
   }
-  else
-{
-//  printf("Sent %lu packets\n", cur_count);
-}
 }
 
 int main(int argc, char * argv[])
